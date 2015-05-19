84d49cc53b6dbade5ec7ef2f90ea459b697961f3
wangling <wb-wanglingwl@alibaba-inc.com>
2015-05-19 20:29:56 +0800

    add grunt watching less

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js']
+    },
+
+    copy: {
+    },
+
+    uglify: {
+    },
+
+    less: {
+      dev: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: [
+        {
+          expand: true,
+          ext: '.css',
+          src: 'common/**/*.less'
+        },
+        {
+          expand: true,
+          ext: '.css',
+          src: 'src/**/*.less'
+        }]
+      }
+    },
+
+    watch: {
+      styles: {
+        files: ['common/**/*.less', 'src/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    },
+
+  });
+
+  // 加载任务插件。
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  // 默认被执行的任务列表。
+  grunt.registerTask('default', ['copy', 'uglify']);
+
+};
